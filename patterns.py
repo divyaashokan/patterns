5dd9209f07842633d3de72e0d299d3e66ecbf2e1
divyaashokan <divyashok.905@gmail.com>
2024-10-16 22:50:08 +0530

    feat: diamond program is added

--- patterns.py
+++ patterns.py
@@ -66,4 +66,24 @@ def pattern_reverse_hill(n):
         for l in range(n-i):
             print("*",end=" ")
         print()
-pattern_reverse_hill(5)                  
+pattern_reverse_hill(5) 
+print()
+#diamond
+def diamond(n):
+    for i in range(n-1):
+        for j in range(n-i):
+            print(" ",end=" ")
+        for k in range(i):
+            print("*",end=" ")
+        for  l in range(i+1):
+            print("*",end=" ")
+        print()
+    for i in range(n):
+        for j in range(i+1):
+            print(" ",end=" ")
+        for k in range(i,n-1):
+            print("*",end=" ")
+        for l in range(n-i):
+            print("*",end=" ")
+        print()
+diamond(5)        
